28ad498eebe457fa0f62025b2fe7bde96cc79dfb
koralm <koralm.for@gmail.com>
2016-01-13 18:27:52 +0100

    Poprawiony wygląd zrobić wzór formularza :)

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ var users = require('./routes/users');
 
 var app = express();
 
+//NEW ADD
+//MONGO DB
+var mongoose = require('mongoose');
+var work_card = require('./models/work_card');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,6 +31,9 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+//MOONGOSE
+mongoose.connect('mongodb://188.125.237.131:5081/Klub_MW');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Klub Młodego Wynalazcy', dupa: 5 });
 });
 
 module.exports = router;
