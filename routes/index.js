322cb2238f29f64d55a1112c332a234831427287
koralm <koralm.for@gmail.com>
2016-01-14 01:50:30 +0100

    Ogarnąć POST

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Klub Młodego Wynalazcy', dupa: 5 });
 });
 
+/* GET settings/options page. */
+router.get('/options', function(req, res, next) {
+  res.render('settings/options', { title: 'Klub Młodego Wynalazcy', dupa: 5 });
+});
+
+/* GET settings/options/add page. */
+router.get('/add', function(req, res, next) {
+  res.render('settings/worksheet/add', { title: 'Klub Młodego Wynalazcy', dupa: 5 });
+});
+
 module.exports = router;
